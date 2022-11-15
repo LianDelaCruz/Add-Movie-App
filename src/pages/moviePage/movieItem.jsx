3e3838335306712e9c5adf62d3e89edce7fce120
Takikato09 <liandelacruz09@hotmail.com>
2022-11-15 15:33:19 +0100

    adds navigation and location testing
    in the movie pages

--- src/pages/moviePage/movieItem.jsx
+++ src/pages/moviePage/movieItem.jsx
@@ -1,5 +1,11 @@
+import {useLocation} from "react-router-dom";
 const MovieItem = () => {
-    console.log('movie item page')
+    const location = useLocation();
+    console.log(location.state?.movieItem)
+    console.log(location)
+    return(
+        <div></div>
+    )
 }
 
 export default MovieItem;
--- src/pages/moviePage/movies.jsx
+++ src/pages/moviePage/movies.jsx
@@ -2,27 +2,26 @@ import { useState } from "react";
 import { getMovies } from "../../helpers/localStorage";
 import { useNavigate } from "react-router-dom";
 
-
 const Movies = (props) => {
-  //const navigate = useNavigate();
-  const [movies, setMovies] = useState(getMovies());
+  const navigate = useNavigate();
+  const movies = getMovies();
   console.log("submit button for add movie success!");
 
-  const navigate = useNavigate();
-
+  const renderMovieItem = (movieItem) => {
+    navigate(`/movies/` + movieItem.title, { state: { movieItem } });
+  };
   return (
     <div>
       <ul className="movie-list-container">
         {movies.map((movieItem, index) => {
-          const renderMovieItem = () => {
-            navigate(`/movies/` + movieItem.title, { state: { movieItem } });
-          };
           return (
             <li key={index}>
               <h1>Title: {movieItem.title}</h1>
               <img alt="img" src={movieItem.img}></img>
               <h2>Short Description: {movieItem.shortInfo}</h2>
-              <button onClick={renderMovieItem}>Read more </button>
+              <button onClick={() => renderMovieItem(movieItem)}>
+                Read more{" "}
+              </button>
             </li>
           );
         })}
