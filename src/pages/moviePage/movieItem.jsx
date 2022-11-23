b37a334913dbf040191a3e8d7bd897d6951ee302
Takikato09 <liandelacruz09@hotmail.com>
2022-11-23 22:52:29 +0100

    CSS styling part 2

--- src/pages/moviePage/movieItem.jsx
+++ src/pages/moviePage/movieItem.jsx
@@ -3,19 +3,19 @@ const MovieItem = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const goBackToMovies = () => {
-    navigate(-1)
-  }
+    navigate(-1);
+  };
 
   return (
-    <>
-    <div>
-      <h1>{location.state?.movieItem.title}</h1>
-      <img src={location.state.movieItem.img}></img>
-      <h3>{location.state.movieItem.shortInfo}</h3>
-      <p>{location.state.movieItem.longInfo}</p>
+    <div className="mv-item-cont">
+      <h1 className="font-size">{location.state?.movieItem.title}</h1>
+      <img className="mv-img-itm" src={location.state.movieItem.img}></img>
+      <h3 className="font-size">{location.state.movieItem.shortInfo}</h3>
+      <p className="font-size">{location.state.movieItem.longInfo}</p>
+      <button className="go-back-btn" onClick={goBackToMovies}>
+        Go Back
+      </button>
     </div>
-    <button onClick={goBackToMovies}>Go Back</button>
-    </>
   );
 };
 
