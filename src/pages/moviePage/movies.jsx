bf4ba1b62a6ff16db92b3f924b811d956038a83c
Takikato09 <liandelacruz09@hotmail.com>
2022-11-23 23:01:40 +0100

    styling

--- src/pages/moviePage/movies.jsx
+++ src/pages/moviePage/movies.jsx
@@ -1,4 +1,3 @@
-
 import { getMovies } from "../../helpers/localStorage";
 import { useNavigate } from "react-router-dom";
 import { getLoggedInUser, deleteMovie } from "../../helpers/localStorage";
@@ -9,30 +8,38 @@ const Movies = (props) => {
   const navigate = useNavigate();
   const [movies, setMovies] = useState(getMovies());
   const currentUser = getLoggedInUser(authorizedUser);
-  
+
   const goToMoviePage = (movieItem) => {
     navigate(`/movies/` + movieItem.title, { state: { movieItem } });
   };
 
   const handleDeleteClick = (title) => {
-    deleteMovie(title)
-    setMovies(getMovies())
-  }
+    deleteMovie(title);
+    setMovies(getMovies());
+  };
 
   return (
     <div>
       <ul className="movie-list-container">
         {movies.map((movieItem) => {
           return (
-            <li key={movieItem.title} className="list-cont" >
+            <li key={movieItem.title} className="list-cont">
               <h2 className="movie-font">{movieItem.title}</h2>
               <img className="color" alt="img" src={movieItem.img}></img>
               <h4 className="movie-font">{movieItem.shortInfo}</h4>
-              <button className="movie-button" onClick={() => goToMoviePage(movieItem)}>
+              <button
+                className="movie-button"
+                onClick={() => goToMoviePage(movieItem)}
+              >
                 Read more
               </button>
               {currentUser === movieItem.user && (
-                <button className="movie-button" onClick={() => handleDeleteClick(movieItem.title)}>Delete</button>
+                <button
+                  className="movie-button"
+                  onClick={() => handleDeleteClick(movieItem.title)}
+                >
+                  Delete
+                </button>
               )}
             </li>
           );
